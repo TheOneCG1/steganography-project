e55314bb31277897b862f628e36f444b9148a738
Calvin Gott <clgott@memphis.edu>
2024-04-04 18:53:06 -0500

    New Change to Switch

--- ui_more/lsb.py
+++ ui_more/lsb.py
@@ -1,24 +1,17 @@
 #!/usr/bin/env python3
 import stepic
 from PIL import Image
-from cryptography.fernet import Fernet 
 
 print(" ... Hide Away ...")
 
-key = Fernet.generate_key()
-print(key)
-enc = Fernet(key)
-
-Data = input(" Data: ")
-data_enc = enc.encrypt(Data.encode())
-
-file = input(" PHOTO: ")
+Data = input("Data: ")
+file = input("PHOTO: ")
 
 img = Image.open(file)
-img_stegano = stepic.encode(img, data_enc)
+img_stegano = stepic.encode(img, Data.encode())
 
-img_stegano.save("steggg.png")
+img_stegano.save("somenew.jpeg")
 
 print("*--------------*")
 
-input(" FIN (press enter -> exit) ")
+input("FIN (press enter -> exit) ")
